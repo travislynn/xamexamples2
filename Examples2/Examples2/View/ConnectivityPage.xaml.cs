1525b6b97863bf2907e240fa266a3fb003212ea0
Travis Lynn <travis.lynn@veteransunited.com>
2017-12-23 05:22:50 -0600

    Add new page for new example work.

---
+++ Examples2/Examples2/View/ConnectivityPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Examples2.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examples2.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ConnectivityPage : ContentPage
+    {
+
+        private ConnectivityPageViewModel vm;
+
+        public ConnectivityPage()
+        {
+            InitializeComponent();
+            vm = new ConnectivityPageViewModel();
+            BindingContext = vm;
+        }
+
+
+    }
+}
